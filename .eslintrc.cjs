4b48438c87a5a7e0d6c71e3eeb0c621c676b1593
袁志杰 <88603076@qq.com>
2022-10-17 18:00:56 +0800

    chore: eslint vue3 rules docs finished read

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,19 +2,20 @@ module.exports = {
   root: true,
   globals: {
     defineEmits: 'readonly',
-    defineProps: 'readonly',
+    defineProps: 'readonly'
   },
   plugins: ['@typescript-eslint/eslint-plugin'],
   extends: [
     'airbnb-base',
     'plugin:import/recommended',
     'plugin:vue/vue3-recommended',
-    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended'
   ],
   parser: 'vue-eslint-parser',
   parserOptions: {
-    parser: '@typescript-eslint/parser',
-    ecmaVersion: 2020,
+    parser: '@typescript-eslint/parser', // 解决引入 type {} 报错问题
+    sourceType: 'module',
+    ecmaVersion: 2020
   },
   rules: {
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
@@ -25,7 +26,7 @@ module.exports = {
     indent: [
       'error',
       2,
-      { MemberExpression: 0, SwitchCase: 1, ignoredNodes: ['TemplateLiteral'] },
+      { MemberExpression: 0, SwitchCase: 1, ignoredNodes: ['TemplateLiteral'] }
     ],
     quotes: ['error', 'single'],
     'comma-dangle': ['error', 'always-multiline'],
@@ -35,7 +36,7 @@ module.exports = {
     'linebreak-style': 'off',
     'eol-last': 'off',
     'no-shadow': 'off',
-    'no-unused-vars': 'warn',
+    // 'no-unused-vars': 'warn',
     'arrow-parens': 'off',
     semi: ['error', 'always'],
     eqeqeq: 'off',
@@ -84,22 +85,76 @@ module.exports = {
     '@typescript-eslint/no-explicit-any': 'off',
     '@typescript-eslint/ban-types': 'off',
 
-    // vue
-    'vue/require-component-is': 'off',
-    'vue/valid-v-for': 'off',
-    'vue/html-indent': ['error', 2],
-    'vue/html-self-closing': 'off',
+    // vue link:https://eslint.vuejs.org/rules/
     'vue/max-attributes-per-line': [
       'warn',
       {
         singleline: {
-          max: 3,
+          max: 3
         },
         multiline: {
-          max: 1,
-        },
-      },
+          max: 1
+        }
+      }
     ],
-    'vue/singleline-html-element-content-newline': 'off',
-  },
+    'vue/require-explicit-emits': [
+      'error',
+      {
+        allowProps: true
+      }
+    ],
+    'vue/v-on-event-hyphenation': [
+      'error',
+      'always',
+      {
+        autofix: true
+      }
+    ],
+    'vue/define-macros-order': [
+      'error',
+      {
+        order: ['defineProps', 'defineEmits']
+      }
+    ],
+    'vue/html-comment-content-newline': 'warn',
+    'vue/html-comment-content-spacing': 'warn',
+    'vue/html-comment-indent': 'warn',
+    'vue/match-component-file-name': 'warn',
+    'vue/match-component-import-name': 'warn',
+    'vue/no-multiple-objects-in-class': 'warn',
+    'vue/no-potential-component-option-typo': [
+      'warn',
+      {
+        presets: ['vue', 'vue-router'],
+        threshold: 5
+      }
+    ],
+    'vue/no-ref-object-destructure': 'warn',
+    'vue/no-required-prop-with-default': [
+      'warn',
+      {
+        autofix: true
+      }
+    ],
+    'vue/no-this-in-before-route-enter': 'error',
+    'vue/no-undef-properties': 'warn',
+    'vue/no-unused-refs': 'warn',
+    'vue/no-useless-mustaches': [
+      'warn',
+      {
+        ignoreIncludesComment: false,
+        ignoreStringEscape: true
+      }
+    ],
+    'vue/no-useless-v-bind': 'warn',
+    'vue/no-v-text': 'error',
+    'vue/padding-line-between-blocks': 'warn',
+    'vue/padding-line-between-tags': 'warn',
+    'vue/prefer-separate-static-class': 'warn',
+    'vue/prefer-prop-type-boolean-first': 'error',
+    'vue/prefer-true-attribute-shorthand': 'warn',
+    'vue/require-direct-export': 'error',
+    'vue/v-for-delimiter-style': 'warn',
+    'vue/v-on-function-call': 'warn'
+  }
 };
